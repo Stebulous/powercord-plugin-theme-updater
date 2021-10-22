4ca0f913eb13dcf41b42b3a5ba45e4c530a699c5
xxxcept <30682549+skirtsandflirts@users.noreply.github.com>
2021-10-23 00:58:10 +1100

    Add files via upload

---
+++ powercord-multi-updater.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+from time import sleep
+
+print("---> xxxcept | im gonna update your stuff")
+
+try:
+    if not os.path.exists(R"src\Powercord\plugins"):
+        raise NotADirectoryError("Failed to find powercord directory!")
+
+    if not os.path.exists(R"src\Powercord\themes"):
+        raise NotADirectoryError("Failed to find powercord directory!")
+
+    print("Updating Plugins...\n")
+    os.chdir("src\Powercord\plugins")
+
+    for plugin in os.listdir():
+        if plugin == ".exists":
+            break
+
+        os.chdir(plugin)
+        os.system("git pull")
+        os.chdir("..")
+
+    print("Plugins updated.\n")
+    os.chdir("..")
+
+    print("Updating Themes...\n")
+    os.chdir("themes")
+
+    for theme in os.listdir():
+        if theme == ".exists":
+            break
+
+        os.chdir(theme)
+        os.system("git pull")
+        os.chdir("..")
+
+    print("Themes updated.\n")
+except Exception as err:
+    print(err)
+    sleep(5)
+    quit(1)
