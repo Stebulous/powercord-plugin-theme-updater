d91c7249b0afc8f2622427e22429561175baf09b
Dosage <sky34416@gmail.com>
2022-02-21 21:42:13 -0500

    Added logs
    Added logging for each action as well as counters for your plugins & themes. Now you know exactly what is happening when it is happening :)

--- powercord-multi-updater.py
+++ powercord-multi-updater.py
@@ -2,7 +2,20 @@ import os
 from time import sleep
 from tkinter import Tk, filedialog
 
-print(r"Powercord Theme/Plugin Updater")
+print(r'''  _______ _                             _______  _             _       
+ |__   __| |                           / /  __ \| |           (_)      
+    | |  | |__   ___ _ __ ___   ___   / /| |__) | |_   _  __ _ _ _ __  
+    | |  | '_ \ / _ \ '_ ` _ \ / _ \ / / |  ___/| | | | |/ _` | | '_ \ 
+    | |  | | | |  __/ | | | | |  __// /  | |    | | |_| | (_| | | | | |
+    |_|  |_| |_|\___|_| |_| |_|\___/_/   |_|    |_|\__,_|\__, |_|_| |_|
+         _    _ _____  _____       _______ ______ _____   __/ |        
+        | |  | |  __ \|  __ \   /\|__   __|  ____|  __ \ |___/         
+        | |  | | |__) | |  | | /  \  | |  | |__  | |__) |              
+        | |  | |  ___/| |  | |/ /\ \ | |  |  __| |  _  /               
+        | |__| | |    | |__| / ____ \| |  | |____| | \ \               
+         \____/|_|    |_____/_/    \_\_|  |______|_|  \_\              
+                                                                       
+                                                                       ''')
 
 root = Tk()
 root.withdraw()
@@ -17,32 +30,59 @@ try:
     if not (os.path.exists(themes_dir) and os.path.exists(plugins_dir)):
         raise NotADirectoryError("Failed to find powercord directory!")
 
-    print("Updating Plugins...\n")
+    print("Finding Plugins...\n")
     os.chdir(plugins_dir)
+    totalplugins = 0
 
     for plugin in os.listdir():
         if plugin == ".exists":
-            break
+            continue
 
+        totalplugins += 1
+
+    print('Plugins found:' + str(totalplugins))
+    sleep(1)
+    print("\nUpdating Plugins!\n")
+
+    for plugin in os.listdir():
+        if plugin == ".exists":
+            continue
+
+        print("\nUpdating plugin: " + plugin + "...\n")
         os.chdir(plugin)
         os.system("git pull")
         os.chdir("..")
 
-    print("Plugins updated.\n")
+    print(str(totalplugins) + " Plugins updated.\n")
     os.chdir("..")
 
-    print("Updating Themes...\n")
+    print("Finding themes...\n")
     os.chdir(themes_dir)
+    
+    totalthemes = 0
 
     for theme in os.listdir():
         if theme == ".exists":
-            break
+            continue
 
+        totalthemes += 1
+
+    print("Themes found: " + str(totalthemes))
+    sleep(1)
+    print("\nUpdating Themes!\n")
+
+    for theme in os.listdir():
+        if theme == ".exists":
+            continue
+
+        print("\nUpdating theme: " + theme + "...\n")
         os.chdir(theme)
         os.system("git pull")
         os.chdir("..")
 
-    print("Themes updated.\n")
+    print(str(totalthemes) + " Themes updated.\n")
+    print("\n\n All done!")
+    sleep(5)
 except Exception as err:
     print(err)
     sleep(5)
